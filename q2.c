612a9251daeb9dd3840f74fcdbfb7224f7b75e92
root <root@DESKTOP-N70B5ES.>
2025-03-09 19:26:41 +0500

    Added parallel function with OpenMP

--- q2.c
+++ q2.c
@@ -16,3 +16,30 @@ int main() {
             arr[i][j] = rand() % 1000000; // Assign random values to array elements
         }
     }
+    // Function to find the max element using OpenMP with reduction and scheduling
+int findMaxParallel(int arr[ROWS][COLS], char scheduleType) {
+    int maxElement = arr[0][0]; // Initialize maxElement with the first element
+
+    // Choose scheduling type
+    if (scheduleType == 's') {  // Static scheduling
+        #pragma omp parallel for collapse(2) schedule(static) reduction(max : maxElement)
+        for (int i = 0; i < ROWS; ++i) {
+            for (int j = 0; j < COLS; ++j) {
+                if (arr[i][j] > maxElement) {
+                    maxElement = arr[i][j];
+                }
+            }
+        }
+    } else {  // Dynamic scheduling
+        #pragma omp parallel for collapse(2) schedule(dynamic) reduction(max : maxElement)
+        for (int i = 0; i < ROWS; ++i) {
+            for (int j = 0; j < COLS; ++j) {
+                if (arr[i][j] > maxElement) {
+                    maxElement = arr[i][j];
+                }
+            }
+        }
+    }
+    return maxElement;
+}
+
